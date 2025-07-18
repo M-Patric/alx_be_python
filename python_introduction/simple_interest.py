1c8493ce42188484751abb939900a98d5792c381
Patrick Morrison <patrickkofimorrison@gmail.com>
2025-07-18 14:22:37 +0000

    Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+# Variables definition
+principal = 1000
+rate = 0.05
+time = 3
+#Formula for calculating the simple interest
+interest = principal * rate * time 
+#Command
+print ("The simple interest is:", interest)

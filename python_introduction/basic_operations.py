5603b8afebc4bf7dba60695cf081332754ca539f
Patrick Morrison <patrickkofimorrison@gmail.com>
2025-07-18 10:50:43 +0000

    Performing basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+#Performing the Operations
+result_1 = number1 + number2
+result_2 = number1 - number2
+result_3 = number1 * number2
+#Let Print the Results
+print (f"Addition of {number1} and {number2} is {result_1}")
+print (f"Subtraction of {number1} and {number2} is {result_2}")
+print (f"Multiplication of {number1} and {number2} is {result_3}")

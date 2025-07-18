fed1e624ae984abc46cca0a601478aaf09c438e4
Patrick Morrison <patrickkofimorrison@gmail.com>
2025-07-18 15:13:57 +0000

    Calculating the future age in 2050

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+# Calculating the future age of someone
+# Receive input for the current age
+current_age = int(input("How old are you? "))
+#Calculating the age in the year 2050
+age_in_2050 = current_age + 27 
+print(f"In 2050, you will be {age_in_2050} years old.")

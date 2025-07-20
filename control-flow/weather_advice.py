45b82d665600c715e8efff37814fb64b1ba3b9b8
Patrick Morrison <patrickkofimorrison@gmail.com>
2025-07-20 20:39:56 +0000

    Giving weather based recommendations relating to the clothing to put on

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,12 @@
+#make enquiries from the individual about the weather
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+#recommending clothes for the individual to wear
+if weather == "sunny": 
+    print ("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
+    
